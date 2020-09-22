457171ae897eac2e158bd2fa108c2676913616d8
codecaviette <63386589+codecaviette@users.noreply.github.com>
2020-09-21 20:56:51 -0700

    Define Counter component state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,13 +3,22 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state = {                        // class comp Counter holds state 
+        count: 0,
+    };
+    
     render() {                                 // render method has return stmt inside curly braces
         return (                               // within return stmt is jsx
             <div>                              
-                <h1> Hello World! </h1>
+                <span> {this.formatCount()}</span>    
                 <button> Increment </button>
             </div>
         );
+    }
+
+    formatCount() {
+        const { count } = this.state;           // Destructured this.state.count object so can use shortened "count" in next line rather than "this.state.count"
+        return count === 0 ? "Zero" : count;
     }
 }
 
