5e7e95567f09e2378a3f840f6777d3f03d119740
codecaviette <63386589+codecaviette@users.noreply.github.com>
2020-09-26 19:07:28 -0700

    Add additional conditional rendering with logical operator

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,14 +10,15 @@ class Counter extends Component {
     
     //Helper method: Determines what to display depending on whether or not there are tags in the state
     renderTags() {
-        if (this.state.tags.length === 0) return <p>There are no tags</p>;
+        if (this.state.tags.length === 0) return <p>There are no tags</p>;               // This defines conditional rendering
         return <ul> {this.state.tags.map(tag => <li key={tag}> {tag} </li>)} </ul>       // Dynamically build list in React by mapping thru tags array and assigning each item to be a list item
     }
 
     render() {                                 // render method has return stmt inside curly braces         
         return (                               // within return stmt is jsx
-            <div>    
-                { this.renderTags() }
+            <div>   
+                { this.state.tags.length === 0 && "Please create a new tag!" }          {/* another conditional where && looks at first operand and if it's truthy then returns second operand */}
+                { this.renderTags() }          {/* call this hoisted method for conditional rendering */}
             </div>
         );
     }
