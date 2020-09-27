6ecb195f068d11ed43a2ebe7020aa3ccb2dda918
codecaviette <63386589+codecaviette@users.noreply.github.com>
2020-09-26 18:27:21 -0700

    Build dynamic list in React

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,20 +4,24 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {                        // class comp Counter holds state 
-        count: 1,
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3'],
     };
     
     render() {                                 // render method has return stmt inside curly braces
              
         return (                               // within return stmt is jsx
             <div>    
-                <span className={this.getBadgeClasses} > {this.formatCount()}</span>    
+                <span className={this.getBadgeClasses()} > {this.formatCount()}</span>    
                 <button className="btn btn-secondary btn-sm"> Increment </button>
+                <ul>
+                    {this.state.tags.map(tag => <li key={tag}> {tag} </li>)}        {/* Dynamically build list in React by mapping thru tags array and assigning each item to be a list item */}
+                </ul>
             </div>
         );
     }
 
-    // Change color of badge depending on state.count value
+    // Change color of badge depending on this.state.count value
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
         classes += (this.state.count === 0) ? "warning" : "primary";     //if count equals 0, then append "warning" to what classes equals and set that as new value for classes var; otherwise, append "primary"
