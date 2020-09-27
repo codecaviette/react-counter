cbdf4b31940df6e5cbdb6f34cd111f8eb2f2680e
codecaviette <63386589+codecaviette@users.noreply.github.com>
2020-09-26 18:14:46 -0700

    De-bloat render method by moving hoisted styling outside of it

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,16 +8,20 @@ class Counter extends Component {
     };
     
     render() {                                 // render method has return stmt inside curly braces
-        // Change color of badge depending on state.count value 
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";        //if count equals 0, then append "warning" to what classes equals and set that as new value for classes var; otherwise, append "primary"
-    
+             
         return (                               // within return stmt is jsx
             <div>    
-                <span className={classes} > {this.formatCount()}</span>    
+                <span className={this.getBadgeClasses} > {this.formatCount()}</span>    
                 <button className="btn btn-secondary btn-sm"> Increment </button>
             </div>
         );
+    }
+
+    // Change color of badge depending on state.count value
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";     //if count equals 0, then append "warning" to what classes equals and set that as new value for classes var; otherwise, append "primary"
+        return classes;
     }
 
     formatCount() {
